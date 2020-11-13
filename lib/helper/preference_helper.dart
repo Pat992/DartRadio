508cbb83f114b1f3d6ccd0cf2930fea674c19bc3
Patrick Hettich <patrick_hettich@hotmail.com>
2020-11-13 09:53:35 +0100

    Walking skeleton: added helpers

---
+++ lib/helper/preference_helper.dart
@@ -0,0 +1,31 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class PreferenceHelper {
+  SharedPreferences _prefs;
+  bool _isDarkTheme;
+  List<String> _favorites;
+
+  _initPreferences() async {
+    _prefs = await SharedPreferences.getInstance();
+    _isDarkTheme = _prefs.getBool('isDarkTheme');
+    _favorites = _prefs.getStringList('favorites');
+  }
+
+  get isDarkTheme {
+    return _isDarkTheme;
+  }
+
+  get favorites {
+    return _favorites;
+  }
+
+  Future<void> setDarkTheme(bool themeSetting) async {
+    _isDarkTheme = themeSetting;
+    await _prefs.setBool('isDarkTheme', _isDarkTheme);
+  }
+
+  Future<void> setFavorite(List<String> stations) async {
+    _favorites = stations;
+    await _prefs.setStringList('favorites', _favorites);
+  }
+}
