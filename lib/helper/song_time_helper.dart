691c2e67d2151e7ab33774a0a9400e4604ad5a0d
buetze <duschen.pascal@gmail.com>
2020-11-13 13:00:15 +0100

    completed SongTimeHelper Class

---
+++ lib/helper/song_time_helper.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class SongTimeHelper {
+
+  DateTime _startdate;
+  DateTime _enddate;
+  Duration _songDuration;
+
+  SongTimeHelper(startdate, enddate){
+    this._startdate = startdate;
+    this._enddate = enddate;
+    _songDuration = _calculateDuration(this._startdate, this._enddate);
+  }
+
+  get duration(){
+    return _songDuration;
+  }
+
+  Duration _calculateDuration(startTime, endTime){
+    Duration duration = startTime.difference(endTime);
+  }
+
+  Duration timeLeft(){
+    return DateTime.now().difference(_enddate);
+  }
+
+  int minutesLeft(){
+    return timeLeft().inMinutes;
+  }
+
+  int secondsLeft(){
+    return timeLeft().inSeconds - (timeLeft().inMinutes * 60);
+  }
+
+}
