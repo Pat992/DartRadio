d989e405929bffbc15efd34064f78e10b97c4336
Buetze <Buetze@users.noreply.github.com>
2020-11-27 10:03:43 +0000

    deploy: 4c079c82080c627031c8caa14d8f668acc686aca

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,14 +4,14 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "daaa8d9e2f90f3176980b6d60d9f0946",
-"main.dart.js": "4ab97690d2215e29fb4191bb987fbbd8",
-"index.html": "1a48c5a5894fe889ca8a55c9fc29aadd",
-"/": "1a48c5a5894fe889ca8a55c9fc29aadd",
+"main.dart.js": "6d668299b1a944e11e56ba17bf620af3",
+"index.html": "e9b57557618c2a5799ac3c6a980cf79f",
+"/": "e9b57557618c2a5799ac3c6a980cf79f",
 "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/fonts/MaterialIcons-Regular.otf": "1288c9e28052e028aba623321f7826ac",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "b14fcf3ee94e3ace300b192e9e7c8c5d",
-"assets/NOTICES": "fba848b5d4ce800504fa1fa54a54f220",
+"assets/NOTICES": "1b138436e48afbb51c8adceba5434f1e",
 "manifest.json": "435e896db40282eb9b90813608bc6278",
 "favicon.svg": "a59b21814b4a931ab15d7c54cec8f46f",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
