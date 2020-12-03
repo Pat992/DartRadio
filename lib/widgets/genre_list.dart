9cfd020f9ac78ba5d1f71615fb3a6a39f17830f0
quibiq-pap <philip.appenzeller@quibiq.ch>
2020-12-03 15:33:41 +0100

    Genres: fixed multiple api requests

--- lib/widgets/genre_list.dart
+++ lib/widgets/genre_list.dart
@@ -10,34 +10,17 @@ class GenreList extends StatefulWidget {
 }
 
 class _GenreListState extends State<GenreList>{
-  bool _isLoading = false;
 
   @override
   void initState() {
-    callApi();
     super.initState();
   }
 
-  Future<void> callApi() async {
-    setState(() {
-      _isLoading = true;
-    });
-
-    await Provider.of<StationsProvider>(context, listen: false)
-        .getApiInformation();
-
-    setState(() {
-      _isLoading = false;
-    });
-  }
   @override
   Widget build(BuildContext context) {
     final stationsProvider = Provider.of<StationsProvider>(context);
     final List<Genre> genres = stationsProvider.genres;
-
-    return _isLoading
-        ? Center(child: CircularProgressIndicator())
-        : Column(
+    return Column(
       children: <Widget>[
         Expanded(
           child: ListView.builder(
