50f74c2c989b47c26c7f9d89d18b57bdfc2d05e7
Patrick Hettich <patrick_hettich@hotmail.com>
2020-12-04 11:48:00 +0100

    Web: player-footer for webpage

--- lib/widgets/play_main.dart
+++ lib/widgets/play_main.dart
@@ -2,68 +2,94 @@ import 'package:dart_radio/providers/player_provider.dart';
 import 'package:dart_radio/providers/song_provider.dart';
 import 'package:dart_radio/providers/stations_provider.dart';
 import 'package:dart_radio/widgets/time_progress.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:async/async.dart';
 
 class PlayMain extends StatelessWidget {
-
   AsyncMemoizer _memoizer = AsyncMemoizer();
 
   @override
   Widget build(BuildContext context) {
     final stationsProvider = Provider.of<StationsProvider>(context);
-    final songProvider = Provider.of<SongProvider>(context);
     final player = Provider.of<PlayerProvider>(context);
     final currentStation = stationsProvider.currentStation;
 
     return FutureBuilder(
         future: this._memoizer.runOnce(() async {
-      return Provider.of<SongProvider>(context)
-          .setSongByUrl(stationsProvider.currentStation.songUrl);
-    }),
+          return Provider.of<SongProvider>(context)
+              .setSongByUrl(stationsProvider.currentStation.songUrl);
+        }),
         builder: (context, future) {
           if (future.hasData)
-            return Center(
-              child: SingleChildScrollView(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Image.network(
-                      '${stationsProvider.currentStation.image}',
-                      width: MediaQuery.of(context).size.width * 0.8,
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Text('${songProvider.song.artist}'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Text('${songProvider.song.title}'),
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Container(
-                      child: TimeProgress(),
-                    ),
-                    player.isPlaying
-                        ? IconButton(
-                            icon: Icon(Icons.stop),
-                            onPressed: () {
-                              player.stop();
-                            },
-                          )
-                        : IconButton(
-                            icon: Icon(Icons.play_arrow),
-                            onPressed: () {
-                              player.play(currentStation.streamUrl);
-                            },
+            return !kIsWeb
+                ? Center(
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Hero(
+                            tag: stationsProvider.currentStation.displayName,
+                            child: Image.network(
+                              '${stationsProvider.currentStation.image}',
+                              width: MediaQuery.of(context).size.width * 0.8,
+                            ),
                           ),
-                  ],
-                ),
-              ),
-            );
+                          SizedBox(
+                            height: 20,
+                          ),
+                          Container(
+                            child: TimeProgress(),
+                          ),
+                          player.isPlaying
+                              ? IconButton(
+                                  icon: Icon(Icons.stop),
+                                  onPressed: () {
+                                    player.stop();
+                                  },
+                                )
+                              : IconButton(
+                                  icon: Icon(Icons.play_arrow),
+                                  onPressed: () {
+                                    player.play(currentStation.streamUrl);
+                                  },
+                                ),
+                        ],
+                      ),
+                    ),
+                  )
+                : Expanded(
+                    child: Container(
+                      width: MediaQuery.of(context).size.height * 0.9,
+                      height: 200,
+                      padding: EdgeInsets.all(20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Image.network(
+                            '${stationsProvider.currentStation.image}',
+                            height: MediaQuery.of(context).size.height * 0.8,
+                          ),
+                          TimeProgress(),
+                          player.isPlaying
+                              ? IconButton(
+                                  icon: Icon(Icons.stop),
+                                  onPressed: () {
+                                    player.stop();
+                                  },
+                                )
+                              : IconButton(
+                                  icon: Icon(Icons.play_arrow),
+                                  onPressed: () {
+                                    player.play(currentStation.streamUrl);
+                                  },
+                                ),
+                        ],
+                      ),
+                    ),
+                  );
           return Text("loading...");
         });
   }
--- lib/widgets/time_progress.dart
+++ lib/widgets/time_progress.dart
@@ -1,4 +1,5 @@
 import 'package:dart_radio/providers/song_provider.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animation_progress_bar/flutter_animation_progress_bar.dart';
 import 'package:provider/provider.dart';
@@ -14,35 +15,49 @@ class _TimeProgressState extends State<TimeProgress> {
     final songProvider = Provider.of<SongProvider>(context);
     return Center(
       child: Container(
-        width: MediaQuery.of(context).size.width * 0.9,
-        child: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+        width: kIsWeb
+            ? MediaQuery.of(context).size.width * 0.5
+            : MediaQuery.of(context).size.width * 0.9,
+        child: Column(
           children: <Widget>[
-            Text(
-              '${songProvider.timeToFinishFormatted}',
-              style: TextStyle(
-                fontSize: 14,
-              ),
-            ),
+            Text('${songProvider.song.artist}'),
             SizedBox(
-              width: 10,
+              height: 10,
             ),
-            Expanded(
-              child: FAProgressBar(
-                  currentValue: songProvider.percentDone,
-                  animatedDuration: const Duration(milliseconds: 300),
-                  progressColor: Theme.of(context).primaryColor,
-                  backgroundColor: Theme.of(context).accentColor,
-                  size: 5),
-            ),
+            Text('${songProvider.song.title}'),
             SizedBox(
-              width: 10,
+              height: 10,
             ),
-            Text(
-              '${songProvider.durationFormatted}',
-              style: TextStyle(
-                fontSize: 14,
-              ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '${songProvider.timeToFinishFormatted}',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Expanded(
+                  child: FAProgressBar(
+                      currentValue: songProvider.percentDone,
+                      animatedDuration: const Duration(milliseconds: 300),
+                      progressColor: Theme.of(context).primaryColor,
+                      backgroundColor: Theme.of(context).accentColor,
+                      size: 5),
+                ),
+                SizedBox(
+                  width: 10,
+                ),
+                Text(
+                  '${songProvider.durationFormatted}',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
             ),
           ],
         ),
