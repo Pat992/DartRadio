e99586a66c944c4de3cda99702a11cbffc375e9a
Patrick Hettich <50899306+Pat992@users.noreply.github.com>
2020-12-04 14:47:19 +0100

    Update main_screen.dart

--- lib/screens/main_screen.dart
+++ lib/screens/main_screen.dart
@@ -1,5 +1,4 @@
 import 'package:dart_radio/providers/preference_provider.dart';
-import 'package:dart_radio/providers/stations_provider.dart';
 import 'package:dart_radio/screens/genre_screen.dart';
 import 'package:dart_radio/screens/play_screen.dart';
 import 'package:dart_radio/screens/radio_mobile_screen.dart';
@@ -10,35 +9,22 @@ import 'package:provider/provider.dart';
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    Future<void> _initProviders(context) async {
-      await Provider.of<PreferenceProvider>(context).initPreferences();
-      await Provider.of<StationsProvider>(context, listen: false)
-          .getApiInformation();
-    }
+    Provider.of<PreferenceProvider>(context).initPreferences();
 
     return FutureBuilder(
-      future: _initProviders(context),
-      builder: (context, future)
-      {
-        if (future.connectionState == ConnectionState.done){
-          return MaterialApp(
-            debugShowCheckedModeBanner: false,
-            title: 'DartRadio',
-            theme: Provider.of<PreferenceProvider>(context).isDarkTheme
-                ? darkTheme
-                : lightTheme,
-            routes: {
-              RadioMobileScreen.ROUTE_NAME: (context) => RadioMobileScreen(),
-              GenreScreen.ROUTE_NAME: (context) => GenreScreen(),
-              PlayScreen.ROUTE_NAME: (context) => PlayScreen()
-            },
-          );
-        } else {
-          return Center(child: CircularProgressIndicator());
-        }
-
-      },
+      future: Provider.of<PreferenceProvider>(context).initPreferences(),
+      builder: (context, future) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'DartRadio',
+        theme: Provider.of<PreferenceProvider>(context).isDarkTheme
+            ? darkTheme
+            : lightTheme,
+        routes: {
+          RadioMobileScreen.ROUTE_NAME: (context) => RadioMobileScreen(),
+          GenreScreen.ROUTE_NAME: (context) => GenreScreen(),
+          PlayScreen.ROUTE_NAME: (context) => PlayScreen()
+        },
+      ),
     );
   }
 }
